3a1800413a0450f7ec075bd11cc9bccf6618438b
Can Taşlıçukur <can.taslicukur@ozu.edu.tr>
2024-02-19 01:12:33 +0300

    refactor: :construction: implemented diff class to main.py

--- gait/main.py
+++ gait/main.py
@@ -1,13 +1,23 @@
 import typer
+from typing_extensions import Annotated
 
-app = typer.Typer()
+from .diff import diff
 
-
-@app.command()
-def sa():
-    typer.echo("SA")
+app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
-def as_bro():
-    typer.echo("AS")
+def review(
+    target_tree: Annotated[
+        str,
+        typer.Argument(help="Target tree to compare changes."),
+    ] = "origin/main",
+):
+    """
+    Review changes between HEAD and target tree.
+    """
+    print(diff().head(target_tree))
+
+
+if __name__ == "__main__":
+    app()
